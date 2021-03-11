e252ec297ab23ffddf61b73a0969b0fec848d516
Remi KPADONOU <skpadonou@finances.bj>
2021-03-11 15:31:18 +0100

    Correct the root diretory

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/personal-portfolio-angular'));
+app.use(express.static('./dist/Personal-Portfolio-Angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/personal-portfolio-angular/'}),
+    res.sendFile('index.html', {root: 'dist/Personal-Portfolio-Angular/'}),
 );
 
 // Start the app by listening on the default Heroku port
